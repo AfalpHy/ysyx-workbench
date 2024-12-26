f8de26987ff0d1bfff47198b74cb7d605a2e1a24
AfalpHy <736353503@qq.com>
2024-12-26 18:57:46 +0800

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard f38aa2ac97e3d0b0ba4e71c771f004bde8d18201 Merge pull request #41 from OldLiu001/master

--- npc/vsrc/example.v
+++ npc/vsrc/example.v
@@ -1,2 +1,11 @@
-module example();
+module example (
+    input  a,
+    input  b,
+    output f
+);
+  assign f = a ^ b;
+  initial begin
+    // $dumpfile("test.vcd");
+    #10; $finish;
+  end
 endmodule
