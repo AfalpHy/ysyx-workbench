bde86395db7c551c5b226d01f70a28a76b2b4b2b
tracer-ysyx <tracer@ysyx.org>
2025-01-01 23:24:46 +0800

    > compile NEMU ysyx_22040000 张三 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:24:46 up  5:24,  1 user,  load average: 0.27, 0.12, 0.04

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -111,7 +111,8 @@ bool check_wp() {
     Assert(success, "eval failed when checking wp");
     if (result != wp->val) {
       change = true;
-      printf("%s = %lu\n", wp->expr, result);
+      printf("%s old value = %lu, new value = %lu\n", wp->expr, wp->val,
+             result);
       wp->val = result;
     }
   }
@@ -120,8 +121,8 @@ bool check_wp() {
 
 void print_wp() {
   for (WP *wp = head; wp != NULL; wp = wp->next) {
-    printf("watchpoint's index:%d\n", wp->NO);
-    printf("watchpoint's expr:%s\n", wp->expr);
-    printf("watchpoint's current value:%lu\n", wp->val);
+    printf("watchpoint's index:\t%d\n", wp->NO);
+    printf("watchpoint's expr:\t%s\n", wp->expr);
+    printf("watchpoint's current value:\t%lu\n", wp->val);
   }
 }
