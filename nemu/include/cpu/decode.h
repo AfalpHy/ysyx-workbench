3b3ddf3f938f401a1368848af791651270563a50
tracer-ysyx <tracer@ysyx.org>
2025-01-02 13:06:35 +0800

    > compile NEMU ysyx_22040000 张三 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:06:35 up  3:51,  1 user,  load average: 0.12, 0.11, 0.09

--- nemu/include/cpu/decode.h
+++ nemu/include/cpu/decode.h
@@ -27,7 +27,7 @@ typedef struct Decode {
 } Decode;
 
 // --- pattern matching mechanism ---
-__attribute__((always_inline))
+// __attribute__((always_inline))
 static inline void pattern_decode(const char *str, int len,
     uint64_t *key, uint64_t *mask, uint64_t *shift) {
   uint64_t __key = 0, __mask = 0, __shift = 0;
