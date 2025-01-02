9b9bca645f3778e3817e44d322c236354f1ff82f
tracer-ysyx <tracer@ysyx.org>
2025-01-02 18:50:00 +0800

    > sim RTL ysyx_22040000 张三 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:50:00 up  9:35,  1 user,  load average: 0.03, 0.03, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,30 +1,18 @@
-#include "Vexample.h"
+#include "VNPC.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+static TOP_NAME* top = nullptr;
+
 int main(int argc, char **argv) {
   VerilatedContext *contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
-  Vexample *top = new Vexample{contextp};
-  contextp->traceEverOn(true);
-  VerilatedVcdC *tfp = new VerilatedVcdC;
-  top->trace(tfp, 99);
-  tfp->open("simx.vcd");
+  top = new TOP_NAME{contextp};
   while (!contextp->gotFinish()) {
-    int a = rand() & 1;
-    int b = rand() & 1;
-    top->a = a;
-    top->b = b;
-    top->eval();
-    tfp->dump(contextp->time());
-    contextp->timeInc(1);
-    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-    assert(top->f == (a ^ b));
   }
-  tfp->close();
   delete top;
   delete contextp;
   return 0;
---
+++ npc/vsrc/NPC.v
@@ -0,0 +1,5 @@
+module NPC (
+    input  clk,
+    input  reset
+);
+endmodule
