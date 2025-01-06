23231deb860649b672fb86594808eab770828a13
tracer-ysyx <tracer@ysyx.org>
2025-01-06 17:48:35 +0800

    > sim RTL ysyx_22040000 张三 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:48:35 up  9:08,  1 user,  load average: 0.04, 0.11, 0.15

--- npc/vsrc/Mux.v
+++ npc/vsrc/Mux.v
@@ -24,6 +24,7 @@ module MuxKeyInternal #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1, HAS_DEFAULT = 0) 
   reg hit;
   integer i;
   always @(*) begin
+    $display("key %d",key);
     lut_out = 0;
     hit = 0;
     for (i = 0; i < NR_KEY; i = i + 1) begin
