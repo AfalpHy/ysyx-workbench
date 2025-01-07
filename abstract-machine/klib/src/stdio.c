50374306da628de2a0e4f54fe829836ed47c22f6
tracer-ysyx <tracer@ysyx.org>
2025-01-07 17:59:53 +0800

    > compile NEMU ysyx_22040000 张三 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:59:53 up  8:21,  1 user,  load average: 0.00, 0.04, 0.06

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,24 +5,146 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+static char buff[8192];
+
+static void get_num(char **out, uint64_t num, bool is_sign, int width,
+                    int radix) {
+  uint64_t tmp = num;
+  if (is_sign) {
+    if ((int64_t)tmp < 0) {
+      *(*out)++ = '-';
+      tmp = ~tmp + 1;
+    }
+  }
+  char num_buff[20];
+  int index = 0;
+  do {
+    if (radix == 0) { // 10进制
+      num_buff[index++] = tmp % 10 + '0';
+      tmp /= 10;
+    } else if (radix == 1) { // 16进制
+      char c = tmp % 16;
+      if (c >= 10) {
+        c = c - 10 + 'a';
+      } else {
+        c += '0';
+      }
+      num_buff[index++] = c;
+      tmp /= 16;
+    }
+  } while (tmp != 0);
+  if (radix == 1) {
+    *(*out)++ = '0';
+    *(*out)++ = 'x';
+  }
+  // 填充前置0
+  while (width-- > index) {
+    *(*out)++ = '0';
+  }
+  while (index > 0) {
+    *(*out)++ = num_buff[--index];
+  }
+}
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  char *tmp = buff;
+  int len = vsprintf(buff, fmt, ap);
+  va_end(ap);
+  while (*tmp) {
+    putch(*tmp++);
+  }
+  return len;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  char *result = out;
+  int d;
+  int64_t ld;
+  char c;
+  char *s;
+  int width = 0;
+  uint32_t p;
+  bool skip = false;
+  while (*fmt) {
+    if (*fmt != '%' && !skip) {
+      *out++ = *fmt;
+    } else {
+      skip = false;
+      switch (*++fmt) {
+      case 's':
+        s = va_arg(ap, char *);
+        strcpy(out, s);
+        out += strlen(s);
+        break;
+      case 'd':
+        d = va_arg(ap, int);
+        get_num(&out, d, true, width, 0);
+        width = 0;
+        break;
+      case 'c':
+        c = (char)va_arg(ap, int);
+        *out++ = c;
+        break;
+      case 'p':
+        p = (uint32_t)va_arg(ap, void *);
+        get_num(&out, p, false, width, 1);
+        width=0;
+        break;
+      case 'l': {
+        char ch = *++fmt;
+        if (ch == 'd') {
+          ld = va_arg(ap, int64_t);
+          get_num(&out, ld, true, width, 0);
+          width = 0;
+        }
+        break;
+      }
+      case '0': {
+        char ch = *++fmt;
+        while (ch >= '0' && ch <= '9') {
+          width = width * 10 + ch - '0';
+          ch = *++fmt;
+        }
+        fmt -= 2;
+        skip = true;
+        break;
+      }
+      default:
+        break;
+      }
+    }
+    fmt++;
+  }
+  *out = '\0';
+  return strlen(result) + 1;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int len = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return len;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int len = vsnprintf(out, n, fmt, ap);
+  va_end(ap);
+  return len;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  int len = vsprintf(buff, fmt, ap);
+  if (len <= n) {
+    strcpy(out, buff);
+    return len;
+  }
+  strncpy(out, buff, n);
+  return n;
 }
 
 #endif
