3dcba62a76a1f5610fcd712e2e45ed61f50c974c
AfalpHy <736353503@qq.com>
2025-01-08 12:43:24 +0800

    implement difftest

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,15 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  for (size_t i = 0; i < 32; i++) {
+    if (cpu.gpr[i] != ref_r->gpr[i]) {
+      printf("difference reg name:%s  pc:" FMT_PADDR " dut:" FMT_WORD
+             " ref:" FMT_WORD "\n",
+             pc, reg_name(i), cpu.gpr[i], ref_r->gpr[i]);
+      return false;
+    }
+  }
+  return true;
 }
 
 void isa_difftest_attach() {
