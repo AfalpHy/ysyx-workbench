296f29b5d38d54be3511bae3c158d01988926c83
tracer-ysyx <tracer@ysyx.org>
2025-01-09 08:49:57 +0800

    > compile NEMU ysyx_22040000 张三 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:49:57 up  1:43,  1 user,  load average: 0.09, 0.09, 0.08

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -31,10 +31,6 @@ ifdef TEST_EXPR
 override ARGS += --test-expr
 endif
 
-ifdef BATCH
-override ARGS += --batch
-endif
-
 # Command to execute NEMU
 IMG ?=
 ifeq ($(CONFIG_FTRACE),y)
@@ -48,6 +44,7 @@ NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 
 run-env: $(BINARY) $(DIFF_REF_SO)
 
+run: ARGS += -b
 run: run-env
 	$(call git_commit, "run NEMU")
 	$(NEMU_EXEC)
