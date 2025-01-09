a2103aad16f6ed73d37fbfbc3b297fe35c89fe78
tracer-ysyx <tracer@ysyx.org>
2025-01-09 08:52:53 +0800

    > compile NEMU ysyx_22040000 张三 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:52:53 up  1:46,  1 user,  load average: 0.29, 0.20, 0.13

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -44,7 +44,7 @@ NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 
 run-env: $(BINARY) $(DIFF_REF_SO)
 
-run: override ARGS += -b
+run: ARGS += --batch
 run: run-env
 	$(call git_commit, "run NEMU")
 	$(NEMU_EXEC)
