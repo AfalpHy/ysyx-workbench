de289dfa07a5836ce594e062b32f0b4e9b78965e
tracer-ysyx <tracer@ysyx.org>
2025-01-10 16:19:40 +0800

    > compile NEMU ysyx_22040000 张三 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:19:40 up  9:03,  1 user,  load average: 0.43, 0.28, 0.27

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -58,6 +58,10 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+#ifdef DTRACE
+  log_write("read device: %s\t offset:%d len:%d result:" FMT_WORD "\n",
+            map->name, offset, len, ret);
+#endif
   return ret;
 }
 
@@ -67,4 +71,8 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
+#ifdef DTRACE
+  log_write("write device: %s\t offset:%d len:%d data:" FMT_WORD "\n",
+            map->name, offset, len, data);
+#endif
 }
