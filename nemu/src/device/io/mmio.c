4dfeb9d55382fc960a25b27fac09b6d80d9c45fb
tracer-ysyx <tracer@ysyx.org>
2025-01-10 08:40:51 +0800

    > compile NEMU ysyx_22040000 张三 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:40:51 up  1:06,  1 user,  load average: 0.04, 0.05, 0.01

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -59,5 +59,6 @@ word_t mmio_read(paddr_t addr, int len) {
 }
 
 void mmio_write(paddr_t addr, int len, word_t data) {
-  map_write(addr, len, data, fetch_mmio_map(addr));
+  difftest_skip_ref();
+  // map_write(addr, len, data, fetch_mmio_map(addr));
 }
