a4acbbd3c1b5e6f94636ea3d3f75cf8c449d373d
tracer-ysyx <tracer@ysyx.org>
2025-01-10 19:14:22 +0800

    > sim RTL ysyx_22040000 张三 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:14:22 up 11:58,  1 user,  load average: 0.67, 0.29, 0.21

--- npc/csrc/pmem.cpp
+++ npc/csrc/pmem.cpp
@@ -75,7 +75,6 @@ extern "C" void pmem_write(word_t addr, word_t data, int len) {
             addr, len, data);
 #endif
   if (addr == SERIAL_PORT) {
-    printf("here\n");
     putchar(data);
     return;
   }
