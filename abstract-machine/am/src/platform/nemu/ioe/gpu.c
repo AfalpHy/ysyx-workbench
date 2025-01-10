e44bb0fde8dca3502269e51f5d17c41ab6f56021
tracer-ysyx <tracer@ysyx.org>
2025-01-10 17:23:42 +0800

    > compile NEMU ysyx_22040000 张三 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:23:42 up 10:07,  1 user,  load average: 0.02, 0.05, 0.05

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -25,27 +25,27 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  uint32_t screen_size = inl(VGACTL_ADDR);
-  int width = screen_size >> 16;
-  int height = screen_size & 0xffff;
+  // uint32_t screen_size = inl(VGACTL_ADDR);
+  // int width = screen_size >> 16;
+  // int height = screen_size & 0xffff;
 
-  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-  uint32_t *pixels = ctl->pixels;
-  uint32_t fb_offset = (y * width + x) * sizeof(uint32_t);
+  // int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  // uint32_t *pixels = ctl->pixels;
+  // uint32_t fb_offset = (y * width + x) * sizeof(uint32_t);
 
-  int boundary_w = ((x + w) <= width) ? x + w : width;
-  int boundary_h = ((y + h) <= height) ? y + h : height;
-  for (int i = 0; i < boundary_h; i++) {
-    uint32_t write_addr = FB_ADDR + fb_offset;
-    for (int j = 0; j < boundary_w; j++) {
-      outl(write_addr, *pixels++);
-      write_addr += sizeof(uint32_t);
-    }
-    fb_offset += width * sizeof(uint32_t);
-  }
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-  }
+  // int boundary_w = ((x + w) <= width) ? x + w : width;
+  // int boundary_h = ((y + h) <= height) ? y + h : height;
+  // for (int i = 0; i < boundary_h; i++) {
+  //   uint32_t write_addr = FB_ADDR + fb_offset;
+  //   for (int j = 0; j < boundary_w; j++) {
+  //     outl(write_addr, *pixels++);
+  //     write_addr += sizeof(uint32_t);
+  //   }
+  //   fb_offset += width * sizeof(uint32_t);
+  // }
+  // if (ctl->sync) {
+  //   outl(SYNC_ADDR, 1);
+  // }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
