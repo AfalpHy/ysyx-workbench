1821e9f53457b91efce57179e7fc67456c2e792e
tracer-ysyx <tracer@ysyx.org>
2025-01-20 18:40:18 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:40:18 up  2:13,  1 user,  load average: 0.10, 0.06, 0.08

--- npc/vsrc/LSU.v
+++ npc/vsrc/LSU.v
@@ -92,18 +92,21 @@ module ysyx_25010008_LSU (
         end
       end else if (state == HANDLE_WADDR) begin
         if (awready) begin
+              $display("write1");
           awvalid <= 0;
           wvalid  <= 1;
           state   <= HANDLE_WDATA;
         end
       end else if (state == HANDLE_WDATA) begin
         if (wready) begin
+             $display("write2");
           wvalid <= 0;
           bready <= 1;
           state  <= HANDLE_BRESP;
         end
       end else begin
         if (bvalid & !bresp) begin
+              $display("write3");
           bready <= 0;
           write_done <= 1;
           state <= IDLE;
