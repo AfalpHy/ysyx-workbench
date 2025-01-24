65a596862b140d5c2e842362a9571b4c137353a9
tracer-ysyx <tracer@ysyx.org>
2025-01-24 16:56:42 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:56:42 up 17:36,  1 user,  load average: 0.13, 0.12, 0.09

--- npc/vsrc/Arbiter.v
+++ npc/vsrc/Arbiter.v
@@ -179,7 +179,7 @@ module ysyx_25010008_Arbiter (
           end
         end
       end
-        $display("ar",,state,,io_master_bready,,io_master_bvalid);
+        $display("ar",,state,,io_master_rready,,io_master_rvalid,,io_master_bready,,io_master_bvalid);
     end
   end
 
