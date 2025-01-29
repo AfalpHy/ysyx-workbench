4b28d766bd622df143cda0340fb9cc4337679575
tracer-ysyx <tracer@ysyx.org>
2025-01-29 12:25:06 +0800

    > compile NEMU ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:25:06 up  7:43,  1 user,  load average: 0.14, 0.20, 0.14

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -7,7 +7,7 @@ void __am_timer_init() {
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   // must read (rtc_addr + 4) first because time reg will be updated only when
   // offset equals 4
-  uptime->us =inl(RTC_ADDR)  | (uint64_t)inl(RTC_ADDR + 4) << 32;
+  uptime->us = (uint64_t)inl(RTC_ADDR + 4) << 32 | inl(RTC_ADDR);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
