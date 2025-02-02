337c543ace2e48134ad4dd741400336e7c5f5f92
tracer-ysyx <tracer@ysyx.org>
2025-02-02 23:31:38 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:31:38 up 13:42,  1 user,  load average: 0.05, 0.11, 0.08

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -1,13 +1,27 @@
 #include <am.h>
 #include <klib-macros.h>
 
+#define UART_ADDR 0x10000000
+#define UART_REG_TR 0
+#define UART_REG_LC 3
+#define UART_REG_LS 5
+#define UART_REG_DLL 0
+#define UART_REG_DLH 1
+
 int main(const char *args);
 
 static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS
 
+static inline uint8_t  inb(uintptr_t addr) { return *(volatile uint8_t  *)addr; }
 static inline void outb(uintptr_t addr, uint8_t  data) { *(volatile uint8_t  *)addr = data; }
 
-void putch(char ch) { outb(0x10000000, ch); }
+void putch(char ch) {
+  // loop until transmitter is not empty
+  while (!(inb(UART_ADDR + UART_REG_LS) & 0x40)) {
+  }
+
+  outb(UART_ADDR + UART_REG_TR, ch);
+}
 
 void halt(int code) {
   asm volatile("mv a0, %0; ebreak" : : "r"(code));
@@ -17,6 +31,11 @@ void halt(int code) {
 extern char _data,_data_start,_end;
 
 void _trm_init() {
+  // init uart
+  outb(UART_ADDR + UART_REG_LC, 0x80); // set dlab(divisor latch access bit)
+  outb(UART_ADDR + UART_REG_DLL, 12);
+  outb(UART_ADDR + UART_REG_DLH, 0);
+  outb(UART_ADDR + UART_REG_LC, 3); // recover
   // copy data from mrom to sram
   int size = &_end - &_data_start;
   for (int i = 0; i < size; i++) {
