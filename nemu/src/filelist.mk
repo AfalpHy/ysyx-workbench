85877f5638f6aa02fbbb6ffee4d5375422928e9a
tracer-ysyx <tracer@ysyx.org>
2025-02-04 10:55:01 +0800

    > compile NEMU ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:55:01 up  2:31,  1 user,  load average: 0.06, 0.08, 0.10

--- nemu/src/filelist.mk
+++ nemu/src/filelist.mk
@@ -18,7 +18,7 @@ DIRS-y += src/cpu src/monitor src/utils
 DIRS-$(CONFIG_MODE_SYSTEM) += src/memory
 DIRS-BLACKLIST-$(CONFIG_TARGET_AM) += src/monitor/sdb
 DIRS-BLACKLIST-$(CONFIG_TARGET_SHARE) += src/monitor src/device
-SRCS-BLACKLIST-$(CONFIG_TARGET_SHARE) += src/cpu/difftest/dut.c
+SRCS-BLACKLIST-$(CONFIG_TARGET_SHARE) += src/cpu/difftest/dut.c src/utils/elf.c src/utils/log.c
 SRCS-BLACKLIST-$(CONFIG_TARGET_NATIVE_ELF) += src/cpu/difftest/ref.c
 
 SHARE = $(if $(CONFIG_TARGET_SHARE),1,0)
