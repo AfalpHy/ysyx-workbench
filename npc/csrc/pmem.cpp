2e6e4dfce55fec67607357b2840ee2306b6f4509
tracer-ysyx <tracer@ysyx.org>
2025-02-07 14:59:59 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:59:59 up  2:00,  1 user,  load average: 0.33, 0.21, 0.13

--- npc/csrc/pmem.cpp
+++ npc/csrc/pmem.cpp
@@ -20,7 +20,7 @@ extern "C" void flash_read(int32_t addr, int32_t *data) {
   int tmp = mem[addr / 4];
   *data = ((tmp >> 24) & 0xff) | (tmp << 24) | ((tmp & 0xff00) << 8) |
           ((tmp >> 8) & 0xff00);
-  printf("%x", *data);
+  printf("%x\n", *data);
 }
 
 extern "C" void mrom_read(int32_t addr, int32_t *data) {
