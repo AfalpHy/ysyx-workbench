fdaaa0ff8cce2ec112355b02c2713f4151ce1a60
tracer-ysyx <tracer@ysyx.org>
2025-02-08 17:35:10 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:35:10 up  2:23,  1 user,  load average: 0.64, 1.18, 0.89

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -59,16 +59,17 @@ int load_img(const string &filepath) {
 }
 
 int main(int argc, char **argv) {
-  // contextp = new VerilatedContext;
+  contextp = new VerilatedContext;
   Verilated::commandArgs(argc, argv);
   signal(SIGINT, sigint_handler);
   signal(SIGSEGV, sigsegv_handler);
   struct timeval now;
   gettimeofday(&now, NULL);
   begin_us = now.tv_sec * 1000000 + now.tv_usec;
-  // contextp->traceEverOn(true);
+  contextp->traceEverOn(true);
   VerilatedVcdC *tfp = new VerilatedVcdC;
   top.trace(tfp, 99); // 99 是 VCD 文件的详细级别，0 是最低详细级别，99 是最高
+  tfp->open("waveform.vcd"); // 打开 VCD 文件
   // initial
   top.eval();
   
@@ -127,8 +128,7 @@ int main(int argc, char **argv) {
   } else {
     cout << img << "\033[32m\tGOOD TRAP\033[0m" << endl;
   }
-  // tfp->close();
-  tfp->open("waveform.vcd"); // 打开 VCD 文件
+  tfp->close();
   top.final();
   return status;
 }
