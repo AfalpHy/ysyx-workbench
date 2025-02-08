de44c73f4beffc87b780dfca1ec3937598c664e2
tracer-ysyx <tracer@ysyx.org>
2025-02-08 18:26:22 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:26:22 up  3:19,  1 user,  load average: 0.46, 0.74, 0.72

--- npc/vsrc/IFU.v
+++ npc/vsrc/IFU.v
@@ -33,7 +33,7 @@ module ysyx_25010008_IFU (
     set_pc(pc);
     set_write_back(write_back);
     $dumpfile("dump.vcd");
-    // $dumpvars();
+    $dumpvars();
   end
 
   always @(posedge clock) begin
