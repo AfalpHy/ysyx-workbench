1c2dd67034736ac12402112e191c89088e6c8340
tracer-ysyx <tracer@ysyx.org>
2025-02-09 17:46:05 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:46:05 up 49 min,  1 user,  load average: 0.15, 0.14, 0.12

--- npc/csrc/exec.cpp
+++ npc/csrc/exec.cpp
@@ -86,7 +86,7 @@ void single_cycle() {
   extern VerilatedContext contextp;
   contextp.timeInc(1);
   extern VerilatedVcdC *tfp;
-  tfp->dump(contextp.time());
+  // tfp->dump(contextp.time());
   top.clock = 1;
   top.eval();
   tfp->dumpvars(0, "");
