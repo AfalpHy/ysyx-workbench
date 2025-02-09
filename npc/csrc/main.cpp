b8c20967d1d1d4b9e04460ae1c01fe6926b5ebca
tracer-ysyx <tracer@ysyx.org>
2025-02-09 21:48:02 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:48:02 up  2:20,  1 user,  load average: 0.25, 0.47, 0.27

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -19,7 +19,7 @@
 using namespace std;
 
 VerilatedContext contextp;
-VerilatedVcdC *tfp = nullptr;
+VerilatedVcdC tfp;
 TOP_NAME top;
 
 int status = 0;
@@ -44,7 +44,7 @@ void sigint_handler(int sig) {
 }
 void sigsegv_handler(int sig) {
   fflush_trace();
-  tfp->close();
+  tfp.close();
   printf("receive SIGSEGV\n");
   exit(1);
 }
@@ -69,9 +69,8 @@ int main(int argc, char **argv) {
   begin_us = now.tv_sec * 1000000 + now.tv_usec;
 
   contextp.traceEverOn(true);
-  tfp = new VerilatedVcdC;
-  top.trace(tfp, 99);
-  tfp->open("waveform.vcd");
+  top.trace(&tfp, 99);
+  tfp.open("waveform.vcd");
 
   // initial
   top.eval();
@@ -131,6 +130,6 @@ int main(int argc, char **argv) {
   } else {
     cout << img << "\033[32m\tGOOD TRAP\033[0m" << endl;
   }
-  tfp->close();
+  tfp.close();
   return status;
 }
