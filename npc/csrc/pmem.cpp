4c4b1f24bfe10e9fca8129b15ff4289b4533aa8e
tracer-ysyx <tracer@ysyx.org>
2025-02-10 17:18:36 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:18:36 up  9:16,  1 user,  load average: 0.30, 0.23, 0.17

--- npc/csrc/pmem.cpp
+++ npc/csrc/pmem.cpp
@@ -73,3 +73,28 @@ extern "C" void pmem_write(word_t addr, word_t data, int mask) {
   word_t origin_data = *(word_t *)pmem_addr;
   *(word_t *)pmem_addr = (origin_data & ~mask) | (data & mask);
 }
+
+extern "C" void psram_read(paddr_t addr, int *data) {
+  word_t result;
+  result = pmem[addr / 4];
+#ifdef MTRACE
+  extern uint64_t total_insts_num;
+  if (print_mtrace && total_insts_num < 10000)
+    fprintf(log_fp, "read addr:\t" FMT_PADDR "\tdata:" FMT_WORD "\n", addr,
+            result);
+#endif
+  *data = result;
+}
+
+extern "C" void psram_write(word_t addr, word_t data, int mask) {
+#ifdef MTRACE
+  extern uint64_t total_insts_num;
+  if (print_mtrace && total_insts_num < 10000)
+    fprintf(log_fp, "write addr:\t" FMT_PADDR "\tdata:" FMT_WORD "\n", addr,
+            data);
+#endif
+  uint8_t *pmem_addr = (uint8_t *)pmem;
+  pmem_addr += addr;
+  word_t origin_data = *(word_t *)pmem_addr;
+  *(word_t *)pmem_addr = (origin_data & ~mask) | (data & mask);
+}
