4de7ce98d5cda229315413935c111c2e604ef34a
tracer-ysyx <tracer@ysyx.org>
2025-02-11 14:45:32 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:45:32 up  4:51,  1 user,  load average: 0.74, 0.40, 0.23

--- npc/vsrc/LSU.v
+++ npc/vsrc/LSU.v
@@ -108,6 +108,7 @@ module ysyx_25010008_LSU (
           wvalid <= 0;
           bready <= 1;
           state  <= HANDLE_BRESP;
+          if(awaddr>=32'h80000000)
           $display("%h",wdata);
         end
       end else if (state == HANDLE_BRESP) begin
