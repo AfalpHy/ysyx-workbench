6d7b2e5d12a4d36f9af4cbb1d5d6a97a1acce717
tracer-ysyx <tracer@ysyx.org>
2025-02-11 23:11:25 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:11:25 up 13:44,  1 user,  load average: 0.80, 0.85, 0.50

--- npc/csrc/ftrace.cpp
+++ npc/csrc/ftrace.cpp
@@ -116,7 +116,10 @@ void ftrace(word_t pc, word_t addr, uint32_t inst) {
     return;
   }
   char *fun_name = get_fun_name(addr);
-  Assert(fun_name, "ftrace get function name failed");
+  Assert(fun_name,
+         "ftrace get function name failed, pc:" FMT_PADDR " addr:" FMT_PADDR
+         " inst:0x%08x",
+         pc, addr, inst);
   if (is_call(addr)) {
     fprintf(ftrace_log, "[" FMT_PADDR "]", pc);
     for (int i = 0; i < indent; i++) {
