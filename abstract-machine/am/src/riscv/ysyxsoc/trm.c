99d5b07175657d24c209352b6815024a9b5951bb
tracer-ysyx <tracer@ysyx.org>
2025-02-19 18:46:41 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:46:41 up  8:20,  1 user,  load average: 0.63, 0.50, 0.41

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -28,8 +28,8 @@ static inline void outb(uintptr_t addr, uint8_t  data) { *(volatile uint8_t  *)a
 
 void putch(char ch) {
   // loop until transmitter is empty
-  while (!(inb(UART_ADDR + UART_REG_LS) & 0x40)) {
-  }
+  // while (!(inb(UART_ADDR + UART_REG_LS) & 0x40)) {
+  // }
 
   outb(UART_ADDR + UART_REG_TR, ch);
 }
@@ -42,7 +42,7 @@ void halt(int code) {
 void init_uart() {
   outb(UART_ADDR + UART_REG_LC, 0x80); // set dlab(divisor latch access bit)
   outb(UART_ADDR + UART_REG_DLH, 0);
-  outb(UART_ADDR + UART_REG_DLL, 1);
+  outb(UART_ADDR + UART_REG_DLL, 2);
   outb(UART_ADDR + UART_REG_LC, 3); // recover
 }
 
