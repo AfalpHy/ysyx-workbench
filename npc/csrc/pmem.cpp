9fb9de83f539392cff9fc0c1d2664a105befea25
AfalpHy <736353503@qq.com>
2025-02-20 15:03:01 +0800

    modify pmem code

--- npc/csrc/pmem.cpp
+++ npc/csrc/pmem.cpp
@@ -8,7 +8,7 @@
 #define SERIAL_PORT (DEVICE_BASE + 0x00003f8)
 #define RTC_ADDR (DEVICE_BASE + 0x0000048)
 
-word_t pmem[0x10000000] = {};
+word_t flash[0x10000000] = {};
 word_t psram[0x100000] = {};
 bool print_mtrace = false;
 
@@ -17,63 +17,19 @@ extern uint64_t begin_us;
 extern bool skip_ref_inst;
 
 extern "C" void flash_read(int32_t addr, int32_t *data) {
-  int tmp = pmem[addr / 4];
+  int tmp = flash[addr / 4];
   *data = ((tmp >> 24) & 0xff) | (tmp << 24) | ((tmp & 0xff00) << 8) |
           ((tmp >> 8) & 0xff00);
 }
 
-extern "C" void mrom_read(int32_t addr, int32_t *data) {
-  uint8_t *tmp = (uint8_t *)pmem;
-  addr &= ~3;
-  tmp += addr - 0x20000000;
-  *data = *(int32_t *)tmp;
-}
+extern "C" void mrom_read(int32_t addr, int32_t *data) { assert(0); }
 
 extern "C" word_t pmem_read(paddr_t addr) {
-  word_t result;
-  if (addr == RTC_ADDR || addr == RTC_ADDR + 4) {
-    static uint64_t us;
-    if (addr == RTC_ADDR + 4) {
-      struct timeval now;
-      gettimeofday(&now, NULL);
-      us = now.tv_sec * 1000000 + now.tv_usec - begin_us;
-      result = us >> 32;
-    } else {
-      result = us & 0xFFFFFFFF;
-    }
-    skip_ref_inst = true;
-  } else {
-    uint8_t *pmem_addr = (uint8_t *)pmem;
-    pmem_addr += (addr - 0x80000000);
-    result = *(word_t *)pmem_addr;
-  }
-#ifdef MTRACE
-  extern uint64_t total_insts_num;
-  if (print_mtrace && total_insts_num < 10000)
-    fprintf(log_fp, "read addr:\t" FMT_PADDR "\tdata:" FMT_WORD "\n", addr,
-            result);
-#endif
-  return result;
+  // disable in soc
+  return 0;
 }
 
-extern "C" void pmem_write(word_t addr, word_t data, int mask) {
-#ifdef MTRACE
-  extern uint64_t total_insts_num;
-  if (print_mtrace && total_insts_num < 10000)
-    fprintf(log_fp, "write addr:\t" FMT_PADDR "\tdata:" FMT_WORD "\n", addr,
-            data);
-#endif
-  if (addr == SERIAL_PORT) {
-    skip_ref_inst = true;
-    putc(data, stderr);
-    return;
-  }
-
-  uint8_t *pmem_addr = (uint8_t *)pmem;
-  pmem_addr += (addr - 0x80000000);
-  word_t origin_data = *(word_t *)pmem_addr;
-  *(word_t *)pmem_addr = (origin_data & ~mask) | (data & mask);
-}
+extern "C" void pmem_write(word_t addr, word_t data, int mask) { assert(0); }
 
 extern "C" void psram_read(paddr_t addr, int *data) {
   word_t result;
