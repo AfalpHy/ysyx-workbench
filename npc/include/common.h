0fd5ce42fceb82eda3fad958d8aa923afad8cdfa
tracer-ysyx <tracer@ysyx.org>
2025-02-22 17:53:34 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:53:34 up  4:53,  1 user,  load average: 0.18, 0.14, 0.15

--- npc/include/common.h
+++ npc/include/common.h
@@ -57,7 +57,6 @@ static inline void print_performance_info() {
   if (!(cond)) {                                                               \
     extern void fflush_trace();                                                \
     fflush_trace();                                                            \
-    \   
     print_debug_info();                                                        \
     print_performance_info();                                                  \
     printf(format "\n", ##__VA_ARGS__);                                        \
