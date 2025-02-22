5b7806d17514d53d01db85da0cb83f3fcd9af6f9
tracer-ysyx <tracer@ysyx.org>
2025-02-22 21:47:22 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:47:22 up  8:47,  1 user,  load average: 0.08, 0.43, 0.52

--- npc/include/common.h
+++ npc/include/common.h
@@ -45,9 +45,9 @@ static inline void print_debug_info() {
 static inline void print_performance_info() {
   extern uint64_t total_cycles, calc_type, ls_type, csr_type;
   extern uint64_t calc_type_cycles, ls_type_cycles, csr_type_cycles;
-  printf("                calc_type    ls_type    csr_type\n");
+  printf("                calc_type\t\tls_type\t\tcsr_type\n");
   printf("counter:        %ld\t\t%ld\t\t%ld\n", calc_type, ls_type, csr_type);
-  printf("average cycles: %lf\t\t%lf\t\t%lf\n",
+  printf("average cycles: %lf\t%lf\t%lf\n",
          (double)calc_type_cycles / calc_type, (double)ls_type / ls_type_cycles,
          (double)csr_type_cycles / csr_type);
 
