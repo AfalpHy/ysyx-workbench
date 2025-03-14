00a698db09409a7bfd3e8edf6c16faead2a0226f
AfalpHy <736353503@qq.com>
2025-03-14 15:43:59 +0800

    implement icache

--- npc/vsrc/IFU.v
+++ npc/vsrc/IFU.v
@@ -5,6 +5,23 @@ import "DPI-C" function void ifu_record1(
   int npc
 );
 
+`define M 2
+`define N 4
+`define BLOCK_BYTE_SIZE 2 ** `M
+`define BLOCK_WIDTH `BLOCK_BYTE_SIZE * 8
+`define TAG_WIDTH 32 - (`M+`N) 
+`define CACHE_SIZE 2 ** `N
+
+// in cache
+`define VALID_POS `TAG_WIDTH + `BLOCK_WIDTH
+`define CACHE_TAG_RANGE `TAG_WIDTH + `BLOCK_WIDTH - 1 : `BLOCK_WIDTH
+`define CACHE_BLOCK_RANGE `BLOCK_WIDTH - 1 : 0
+
+// in pc
+`define PC_TAG_RANGE 31 : `M + `N
+`define PC_INDEX_RANGE `M + `N -1 : `M
+
+
 module ysyx_25010008_IFU (
     input clock,
     input reset,
@@ -31,31 +48,63 @@ module ysyx_25010008_IFU (
     set_pc(pc);
   end
 
+  reg [`TAG_WIDTH + `BLOCK_WIDTH : 0] cache[0:`CACHE_SIZE-1];
+
+  integer i;
+
+  parameter READ_CACHE = 0;
+  parameter READ_MEMORY = 1;
+  parameter IDLE = 2;
+
+  reg [1:0] state;
+
   always @(posedge clock) begin
     if (reset) begin
+      for (i = 0; i < 16; i = i + 1) begin
+        cache[i][`VALID_POS] <= 0;
+      end
       pc <= 32'h3000_0000;
-      arvalid <= 1;
+      arvalid <= 0;
       rready <= 0;
       ivalid <= 0;
+      state <= READ_CACHE;
     end else begin
-      if (arvalid & arready) begin
-        arvalid <= 0;
-        rready  <= 1;
-      end else if (rready & rvalid) begin
-        if (rresp != 0) begin
-          $display("%h", pc);
-          $finish;
+      if (state == READ_CACHE) begin
+        // sram don't need cache
+        if (pc[31:24] != 8'h0f && cache[pc[`PC_INDEX_RANGE]][`VALID_POS] && cache[pc[`PC_INDEX_RANGE]][`CACHE_TAG_RANGE] == pc[`PC_TAG_RANGE]) begin
+          inst   <= cache[pc[`PC_INDEX_RANGE]][`CACHE_BLOCK_RANGE];
+          ivalid <= 1;
+          state  <= IDLE;
+          ifu_record0();
+        end else begin
+          state   <= READ_MEMORY;
+          arvalid <= 1;
         end
-        rready <= 0;
-        inst   <= rdata;
-        ivalid <= 1;
-        ifu_record0();
-      end else if (ivalid & iready) begin
-        ivalid <= 0;
-      end else if (write_back) begin
-        pc <= npc;
-        arvalid <= 1;
-        ifu_record1(inst, npc);
+      end else if (state == READ_MEMORY) begin
+        if (arvalid & arready) begin
+          arvalid <= 0;
+          rready  <= 1;
+        end else if (rready & rvalid) begin
+          if (rresp != 0) begin
+            $display("%h", pc);
+            $finish;
+          end
+          rready <= 0;
+          inst <= rdata;
+          cache[pc[`PC_INDEX_RANGE]][`CACHE_BLOCK_RANGE] <= rdata;
+          cache[pc[`PC_INDEX_RANGE]][`CACHE_TAG_RANGE] <= pc[`PC_TAG_RANGE];
+          cache[pc[`PC_INDEX_RANGE]][`VALID_POS] <= 1;
+          ivalid <= 1;
+          state <= IDLE;
+        end
+      end else begin
+        if (ivalid & iready) begin
+          ivalid <= 0;
+        end else if (write_back) begin
+          pc <= npc;
+          ifu_record1(inst, npc);
+          state <= READ_CACHE;
+        end
       end
     end
   end
