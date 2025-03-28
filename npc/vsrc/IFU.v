5b63cd03366b3daf01731bec4e6f049a364bea7b
tracer-ysyx <tracer@ysyx.org>
2025-03-28 11:43:34 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:43:34 up  1:30,  1 user,  load average: 0.19, 0.13, 0.10

--- npc/vsrc/IFU.v
+++ npc/vsrc/IFU.v
@@ -6,17 +6,16 @@ import "DPI-C" function void ifu_record1(
 );
 import "DPI-C" function void ifu_record2(int delay);
 
-`define M 2
+`define M 3 
 `define N 4
-`define DATA_BYTE_SIZE 2 ** `M
-`define DATA_WIDTH `DATA_BYTE_SIZE * 8
+`define DATA_WIDTH (2 ** `M) * 8
 `define TAG_WIDTH 32 - (`M + `N) 
+`define CACHE_WIDTH `TAG_WIDTH + `DATA_WIDTH + 1
 `define CACHE_SIZE 2 ** `N
 
 // in cache
 `define VALID_POS `TAG_WIDTH + `DATA_WIDTH
 `define CACHE_TAG_RANGE `TAG_WIDTH + `DATA_WIDTH - 1 : `DATA_WIDTH
-`define CACHE_DATA_RANGE `DATA_WIDTH - 1 : 0
 
 // in pc
 `define PC_TAG_RANGE 31 : `M + `N
@@ -34,13 +33,15 @@ module ysyx_25010008_IFU (
     output reg ivalid,
     input iready,
 
+    output reg [31:0] araddr,
     output reg arvalid,
     input arready,
 
     output reg rready,
     input [31:0] rdata,
     input [1:0] rresp,
-    input rvalid
+    input rvalid,
+    input rlast
 );
 
   // set pointer of pc for cpp
@@ -57,6 +58,13 @@ module ysyx_25010008_IFU (
   parameter IDLE = 2;
 
   reg [1:0] state;
+  wire [`N-1:0] index = pc[`PC_INDEX_RANGE];
+  wire [`TAG_WIDTH-1:0] pc_tag = pc[`PC_TAG_RANGE];
+  wire [`CACHE_WIDTH-1:0] cache_block = cache[index];
+  wire valid = cache_block[`VALID_POS];
+  wire [`TAG_WIDTH-1:0] cache_tag = cache_block[`CACHE_TAG_RANGE];
+
+  assign araddr = {pc[31:`M], 1'b0, pc[`M-2:0]};
 
   always @(posedge clock) begin
     if (reset) begin
@@ -72,8 +80,8 @@ module ysyx_25010008_IFU (
     end else begin
       if (state == READ_CACHE) begin
         // sram don't need cache
-        if (pc[31:24] != 8'h0f && cache[pc[`PC_INDEX_RANGE]][`VALID_POS] && cache[pc[`PC_INDEX_RANGE]][`CACHE_TAG_RANGE] == pc[`PC_TAG_RANGE]) begin
-          inst   <= cache[pc[`PC_INDEX_RANGE]][`CACHE_DATA_RANGE];
+        if (pc[31:24] != 8'h0f && valid && cache_tag == pc_tag) begin
+          inst   <= pc[2] ? cache_block[63:32] : cache_block[31:0];
           ivalid <= 1;
           state  <= IDLE;
           ifu_record0();
@@ -91,17 +99,19 @@ module ysyx_25010008_IFU (
             $display("%h", pc);
             $finish;
           end
-          rready <= 0;
-          inst   <= rdata;
-          if (pc[31:24] != 8'h0f) begin
-            cache[pc[`PC_INDEX_RANGE]][`CACHE_DATA_RANGE] <= rdata;
-            cache[pc[`PC_INDEX_RANGE]][`CACHE_TAG_RANGE] <= pc[`PC_TAG_RANGE];
-            cache[pc[`PC_INDEX_RANGE]][`VALID_POS] <= 1;
+          if (rlast) begin
+            rready <= 0;
+            // updata inst if pc[2] is high
+            if (pc[2]) inst <= rdata;
+            if (pc[31:24] != 8'h0f) cache[index] <= {1'b1, pc_tag, rdata, inst};
+            ivalid <= 1;
+            state  <= IDLE;
+            ifu_record2(delay);
+            delay = 0;
+          end else begin
+            // use inst to save data for write cache if needed
+            inst <= rdata;
           end
-          ivalid <= 1;
-          state  <= IDLE;
-          ifu_record2(delay);
-          delay = 0;
         end
       end else begin
         if (ivalid & iready) begin
