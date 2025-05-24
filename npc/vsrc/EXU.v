047236ec1907ba3367ffb4a74dc0d74309cde126
tracer-ysyx <tracer@ysyx.org>
2025-05-24 19:32:11 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:32:11 up  3:24,  1 user,  load average: 0.22, 0.25, 0.17

--- npc/vsrc/EXU.v
+++ npc/vsrc/EXU.v
@@ -120,7 +120,8 @@ module ysyx_25010008_EXU (
       npc_sel_buffer <= npc_sel;
 
       csr_src_buffer <= csr_src_sel[0] ? exu_r_wdata : csr_src_sel[1] ? forward_data : csr_src;
-      if (alu_operand2_sel[1] && csr_src_sel != 0) $display("%x", pc);
+      if (alu_operand2_sel[1] && csr_src_sel != 0)
+        $display("%x %x %x", pc, csr_src_sel, exu_r_wdata);
 
       exu_r_wdata_sel_buffer <= exu_r_wdata_sel;
       csr_wdata1_sel_buffer <= csr_wdata1_sel;
