246b76f227a150c82138995906321bbe32f1a373
tracer-ysyx <tracer@ysyx.org>
2025-08-02 17:45:14 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:45:14 up  2:51,  1 user,  load average: 0.43, 0.23, 0.15

--- npc/vsrc/IFU.v
+++ npc/vsrc/IFU.v
@@ -142,7 +142,7 @@ module ysyx_25010008_IFU (
       if (state == READ_MEMORY) begin
         delay = delay + 1;
         if (arvalid & arready) begin
-          $display("%h", ifu_pc);
+          $display("%h", araddr);
           arvalid <= 0;
           rready  <= 1;
         end
