aa5d0d1d0122bad16e2e35f06e556073779c84e3
tracer-ysyx <tracer@ysyx.org>
2025-08-03 23:20:56 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:20:56 up  3:45,  1 user,  load average: 0.06, 0.12, 0.16

--- abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
+++ abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
@@ -1,5 +1,5 @@
 void _SSBL();
-void __attribute__((section(".fsbl"))) _fsbl() {
+void __attribute__((section(".entry"))) _fsbl() {
   extern char _SSBL_size[], _SSBL_start[], _sram_start[];
   int loop = (int)_SSBL_size >> 2;
 //   int remain = (int)_SSBL_size & 0x3;
