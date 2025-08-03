ec667047313b266392cb9d1cfc6390ad864db162
tracer-ysyx <tracer@ysyx.org>
2025-08-03 12:26:17 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:26:17 up  5:30,  1 user,  load average: 0.04, 0.12, 0.16

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -40,13 +40,12 @@ void halt(int code) {
 }
 
 void init_uart() {
-  int tmp = inb(UART_ADDR + UART_REG_LC);
-  outb(UART_ADDR + UART_REG_LC, 0x80); // set dlab(divisor latch access bit)
+  uint8_t tmp = inb(UART_ADDR + UART_REG_LC);
+  outb(UART_ADDR + UART_REG_LC,
+       tmp | 0x80); // set dlab(divisor latch access bit)
   outb(UART_ADDR + UART_REG_DLH, 0);
   outb(UART_ADDR + UART_REG_DLL, 1);
-  outb(UART_ADDR + UART_REG_LC, 3); // recover
-  printf("%d\n", tmp);
-
+  outb(UART_ADDR + UART_REG_LC, tmp); // recover
 }
 
 void display_id() {
