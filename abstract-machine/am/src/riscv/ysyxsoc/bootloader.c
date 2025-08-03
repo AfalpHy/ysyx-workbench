bd4030964e1287ed169e52fb354102f379a53f5a
tracer-ysyx <tracer@ysyx.org>
2025-08-04 05:41:24 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  05:41:24 up 42 min,  1 user,  load average: 0.00, 0.04, 0.07

--- abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
+++ abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
@@ -1,10 +1,11 @@
 #include <klib.h>
+
 void _ssbl();
 
 void __attribute__((section(".entry"))) _fsbl() {
-  extern char _SSBL_size[], _SSBL_start[], _sram_start[];
-  uint32_t loop = (uint32_t)_SSBL_size >> 2;
-  uint32_t *origin_addr = (uint32_t *)_SSBL_start;
+  extern char _ssbl_size[], _ssbl_start[], _sram_start[];
+  uint32_t loop = (uint32_t)_ssbl_size >> 2;
+  uint32_t *origin_addr = (uint32_t *)_ssbl_start;
   uint32_t *target_addr = (uint32_t *)_sram_start;
 
   while (loop--) {
@@ -12,7 +13,7 @@ void __attribute__((section(".entry"))) _fsbl() {
     origin_addr++;
     target_addr++;
   }
-  uint32_t remain = (uint32_t)_SSBL_size & 0x3;
+  uint32_t remain = (uint32_t)_ssbl_size & 0x3;
   uint8_t *remain_origin_addr = (uint8_t *)origin_addr;
   uint8_t *remain_target_addr = (uint8_t *)target_addr;
 
@@ -25,9 +26,10 @@ void __attribute__((section(".entry"))) _fsbl() {
 }
 
 void _trm_init();
+
 void __attribute__((section(".ssbl"))) __attribute__((noinline)) _ssbl() {
-  extern char _total_size[], _text_start[], _sdram_start[];
-  uint32_t loop = (uint32_t)_total_size >> 2;
+  extern char _text_size[], _text_start[], _sdram_start[];
+  uint32_t loop = (uint32_t)_text_size >> 2;
   uint32_t *origin_addr = (uint32_t *)_text_start;
   uint32_t *target_addr = (uint32_t *)_sdram_start;
 
@@ -36,7 +38,7 @@ void __attribute__((section(".ssbl"))) __attribute__((noinline)) _ssbl() {
     origin_addr++;
     target_addr++;
   }
-  uint32_t remain = (uint32_t)_total_size & 0x3;
+  uint32_t remain = (uint32_t)_text_size & 0x3;
   uint8_t *remain_origin_addr = (uint8_t *)origin_addr;
   uint8_t *remain_target_addr = (uint8_t *)target_addr;
 
