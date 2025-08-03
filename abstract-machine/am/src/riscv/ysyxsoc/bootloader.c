b055876508381dc64a2c64ff3d3b71056adbb40d
tracer-ysyx <tracer@ysyx.org>
2025-08-03 23:23:17 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:23:17 up  3:47,  1 user,  load average: 0.22, 0.18, 0.18

--- abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
+++ abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
@@ -1,10 +1,12 @@
+#include <klib.h>
+
 void _SSBL();
 void __attribute__((section(".entry"))) _fsbl() {
   extern char _SSBL_size[], _SSBL_start[], _sram_start[];
-  int loop = (int)_SSBL_size >> 2;
-//   int remain = (int)_SSBL_size & 0x3;
-  int *origin_addr = (int*)_SSBL_start;
-  int *target_addr = (int*)_sram_start;
+  uint32_t loop = (uint32_t)_SSBL_size >> 2;
+  //   int remain = (int)_SSBL_size & 0x3;
+  uint32_t *origin_addr = (uint32_t *)_SSBL_start;
+  uint32_t *target_addr = (uint32_t *)_sram_start;
 
   while (loop--) {
     *target_addr = *origin_addr;
