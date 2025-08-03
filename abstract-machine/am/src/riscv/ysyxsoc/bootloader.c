363b7b0f8ddfebdd9092a7a9b81c4c5498fe67eb
AfalpHy <736353503@qq.com>
2025-08-04 06:03:07 +0800

    reduce bootloader execution time

---
+++ abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
@@ -0,0 +1,51 @@
+#include <klib.h>
+
+void _ssbl();
+
+void __attribute__((section(".entry"))) _fsbl() {
+  extern char _ssbl_size[], _ssbl_start[], _sram_start[];
+  uint32_t loop = (uint32_t)_ssbl_size >> 2;
+  uint32_t *origin_addr = (uint32_t *)_ssbl_start;
+  uint32_t *target_addr = (uint32_t *)_sram_start;
+
+  while (loop--) {
+    *target_addr = *origin_addr;
+    origin_addr++;
+    target_addr++;
+  }
+  uint32_t remain = (uint32_t)_ssbl_size & 0x3;
+  uint8_t *remain_origin_addr = (uint8_t *)origin_addr;
+  uint8_t *remain_target_addr = (uint8_t *)target_addr;
+
+  while (remain--) {
+    *remain_target_addr = *remain_origin_addr;
+    remain_origin_addr++;
+    remain_target_addr++;
+  }
+  _ssbl();
+}
+
+void _trm_init();
+
+void __attribute__((section(".ssbl"))) __attribute__((noinline)) _ssbl() {
+  extern char _text_size[], _text_start[], _sdram_start[];
+  uint32_t loop = (uint32_t)_text_size >> 2;
+  uint32_t *origin_addr = (uint32_t *)_text_start;
+  uint32_t *target_addr = (uint32_t *)_sdram_start;
+
+  while (loop--) {
+    *target_addr = *origin_addr;
+    origin_addr++;
+    target_addr++;
+  }
+  uint32_t remain = (uint32_t)_text_size & 0x3;
+  uint8_t *remain_origin_addr = (uint8_t *)origin_addr;
+  uint8_t *remain_target_addr = (uint8_t *)target_addr;
+
+  while (remain--) {
+    *remain_target_addr = *remain_origin_addr;
+    remain_origin_addr++;
+    remain_target_addr++;
+  }
+  _trm_init();
+}
