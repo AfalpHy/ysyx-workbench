22eac1fd0a9768ee5f6017f9dfda65bc04cd0f3c
tracer-ysyx <tracer@ysyx.org>
2025-08-25 19:24:00 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:24:00 up 48 min,  1 user,  load average: 0.00, 0.07, 0.31

--- npc/vsrc/Arbiter.v
+++ npc/vsrc/Arbiter.v
@@ -82,7 +82,7 @@ module ysyx_25010008_Arbiter (
   reg is_clint_addr;
 
   // only one master work at the same time, so its logic can be simplified
-  assign io_master_araddr = state == CHOSE_IFU? araddr_0 : (state == CHOSE_LSU && !is_clint_addr) ? araddr_1 : 0;
+  assign io_master_araddr = state == CHOSE_IFU ? araddr_0 : (state == CHOSE_LSU && !is_clint_addr) ? araddr_1 : 0;
   assign io_master_arvalid = (state == CHOSE_IFU && arvalid_0) | (state == CHOSE_LSU && !is_clint_addr && arvalid_1);
   assign io_master_arlen = state == CHOSE_IFU ? arlen_0 : 8'b0;
   assign io_master_arsize = state == CHOSE_IFU ? 3'b010 : arsize_1;
