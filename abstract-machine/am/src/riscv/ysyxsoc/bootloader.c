ba30b4e1a1dffe76006f2e56360e78959250ad49
tracer-ysyx <tracer@ysyx.org>
2025-08-25 19:01:31 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:01:31 up 26 min,  1 user,  load average: 0.24, 0.37, 0.49

--- abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
+++ abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
@@ -25,16 +25,20 @@ void __attribute__((section(".entry"))) _fsbl() {
   _ssbl();
 }
 
-void __attribute__((section(".ssbl"))) __attribute__((noinline))
-ssbl_memcpy(uint32_t size, uint32_t *origin_addr, uint32_t *target_addr) {
-  uint32_t loop = (uint32_t)size >> 2;
+void _trm_init();
+
+void __attribute__((section(".ssbl"))) __attribute__((noinline)) _ssbl() {
+  extern char _text_size[], _text_start[], _sdram_start[];
+  uint32_t loop = (uint32_t)_text_size >> 2;
+  uint32_t *origin_addr = (uint32_t *)_text_start;
+  uint32_t *target_addr = (uint32_t *)_sdram_start;
 
   while (loop--) {
     *target_addr = *origin_addr;
     origin_addr++;
     target_addr++;
   }
-  uint32_t remain = (uint32_t)size & 0x3;
+  uint32_t remain = (uint32_t)_text_size & 0x3;
   uint8_t *remain_origin_addr = (uint8_t *)origin_addr;
   uint8_t *remain_target_addr = (uint8_t *)target_addr;
 
@@ -43,22 +47,5 @@ ssbl_memcpy(uint32_t size, uint32_t *origin_addr, uint32_t *target_addr) {
     remain_origin_addr++;
     remain_target_addr++;
   }
-}
-
-void _trm_init();
-
-void __attribute__((section(".ssbl"))) __attribute__((noinline)) _ssbl() {
-  extern char _text_size[], _text_start[], _sdram_start[];
-  ssbl_memcpy((uint32_t)_text_size, (uint32_t *)_text_start,
-              (uint32_t *)_sdram_start);
-
-  extern char _rodata_size[], _rodata_start[], _rodata_target[];
-  ssbl_memcpy((uint32_t)_rodata_size, (uint32_t *)_rodata_start,
-              (uint32_t *)_rodata_target);
-
-  extern char _data_size[], _data_start[], _data_target[];
-  ssbl_memcpy((uint32_t)_data_size, (uint32_t *)_data_start,
-              (uint32_t *)_data_target);
-
   _trm_init();
 }
