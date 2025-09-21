94d257c4f9c05b31dc7c745b6eaea6020845f371
tracer-ysyx <tracer@ysyx.org>
2025-09-21 13:17:51 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:17:51 up  7:50,  1 user,  load average: 0.62, 0.40, 0.24

--- npc/vsrc/LSU.v
+++ npc/vsrc/LSU.v
@@ -129,6 +129,7 @@ module ysyx_25010008_LSU (
             store_addr_misaligned <= addr_misaligned;
             // must assert in the same time for sdram axi
             awvalid <= addr_misaligned ? 0 : 1;
+            wvalid <= addr_misaligned ? 0 : 1;
           end
 
 `ifdef __VERILATOR__
@@ -167,8 +168,6 @@ module ysyx_25010008_LSU (
 `endif
 
           awvalid <= 0;
-          wvalid <= 1;
-
         end
 
         if (wvalid & wready) begin
