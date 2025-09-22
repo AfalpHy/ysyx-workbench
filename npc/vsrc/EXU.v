54b0d35135a95e146a5863e9019bd07f4540248e
tracer-ysyx <tracer@ysyx.org>
2025-09-22 18:54:28 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:54:28 up  2:06,  1 user,  load average: 0.21, 0.38, 0.49

--- npc/vsrc/EXU.v
+++ npc/vsrc/EXU.v
@@ -1,93 +1,110 @@
 module ysyx_25010008_EXU (
-    input [31:0] pc,
-    input [ 2:0] npc_sel,
+    input clock,
+    input reset,
+
+    input block,
+
+    input decode_valid,
+    output reg execute_valid,
+    input [31:0] idu_pc,
+    output reg [31:0] exu_pc,
+    input [1:0] npc_sel,
 
     input [31:0] imm,
 
     input [31:0] src1,
     input [31:0] src2,
-    input [ 2:0] r_wdata_sel,
+    input [ 1:0] exu_r_wdata_sel,
 
     input [31:0] csr_src,
-    input csr_wdata1_sel,
-    input csr_wdata2_sel,
+    input [ 1:0] csr_src_sel,
 
-    input [7:0] alu_opcode,
-    input [1:0] alu_operand2_sel,
-
-    input [31:0] mem_rdata,
-
-    output [31:0] npc,
-
+    input  [ 7:0] alu_opcode,
+    input  [ 1:0] alu_operand1_sel,
+    input  [ 3:0] alu_operand2_sel,
     output [31:0] alu_result,
 
-    output [31:0] r_wdata,
-    output [31:0] csr_wdata1,
-    output [31:0] csr_wdata2
+    input [31:0] forward_data,
+    output reg [31:0] wsrc,
+
+    output reg [31:0] exu_npc,
+
+    output reg [31:0] exu_r_wdata,
+    output reg [31:0] csr_wdata,
+
+    input clear_pipeline,
+    output reg is_wrong_prediction
 );
 
-  wire [31:0] alu_operand2;
-
-  wire [31:0] snpc = pc + 4;
-  wire [31:0] dnpc = pc + imm;
-
-  ysyx_25010008_MuxKey #(3, 2, 32) mux_alu_operand2 (
-      alu_operand2,
-      alu_operand2_sel,
-      {
-        {2'b00, src2},
-        {2'b01, imm},  // most I_type inst
-        {2'b10, csr_src}  // csrrs or csrrc
-      }
-  );
+  reg [ 7:0] opcode;
+  reg [31:0] operand1;
+  reg [31:0] operand2;
 
   ysyx_25010008_ALU alu (
-      .opcode  (alu_opcode),
-      .operand1(src1),
-      .operand2(alu_operand2),
+      .opcode  (opcode),
+      .operand1(operand1),
+      .operand2(operand2),
       .result  (alu_result)
   );
 
-  ysyx_25010008_MuxKey #(5, 3, 32) mux_npc (
-      npc,
-      npc_sel,
-      {
-        {3'b000, snpc},
-        {3'b001, dnpc},  // jal
-        {3'b010, alu_result & (~32'b1)},  // jalr
-        {3'b011, alu_result[0] ? dnpc : snpc},  // branch
-        {3'b100, csr_src}  // ecall mret
-      }
-  );
+  reg [31:0] snpc;
+  reg [31:0] dnpc;
+  reg [ 1:0] npc_sel_buffer;
+  reg [ 1:0] exu_r_wdata_sel_buffer;
+  reg [31:0] csr_src_buffer;
+  reg [31:0] exu_npc_buffer;
 
-  ysyx_25010008_MuxKey #(5, 3, 32) mux_r_wdata (
-      r_wdata,
-      r_wdata_sel,
-      {
-        {3'b000, alu_result},
-        {3'b001, snpc},  // jal jalr
-        {3'b010, dnpc}, // auipc 
-        {3'b011, mem_rdata}, // load
-        {3'b100, csr_src} // csrrw csrrs csrrc
-      }
-  );
+  always @(*) begin
+    case (npc_sel_buffer)
+      2'b00: exu_npc_buffer = snpc;
+      2'b01: exu_npc_buffer = dnpc;  // jal
+      2'b10: exu_npc_buffer = alu_result & (~32'b1);  // jalr
+      2'b11: exu_npc_buffer = alu_result[0] ? dnpc : snpc;  // branch
+    endcase
+  end
 
-  ysyx_25010008_MuxKey #(2, 1, 32) mux_csr_wdata1 (
-      csr_wdata1,
-      csr_wdata1_sel,
-      {
-        {1'b0, alu_result},  // csrrw csrrs csrrc 
-        {1'b1, 32'd11}  // ecall
-      }
-  );
-  
-  ysyx_25010008_MuxKey #(2, 1, 32) mux_csr_wdata2 (
-      csr_wdata2,
-      csr_wdata2_sel,
-      {
-        {1'b0, 32'b0},  // not used 
-        {1'b1, pc}  // ecall
-      }
-  );
+  always @(*) begin
+    case (exu_r_wdata_sel_buffer)
+      2'b00: exu_r_wdata = alu_result;
+      2'b01: exu_r_wdata = snpc;  // jal jalr
+      2'b10: exu_r_wdata = dnpc;  // auipc 
+      2'b11: exu_r_wdata = csr_src_buffer;  // csrrw csrrs csrrc
+    endcase
+  end
+
+  wire [31:0] src2_tmp = alu_operand2_sel[2] ? exu_r_wdata : alu_operand2_sel[3] ? forward_data : src2;
+  wire [31:0] csr_src_tmp = csr_src_sel[0] ? alu_result : csr_src_sel[1] ? csr_wdata : csr_src;
+
+  always @(posedge clock) begin
+    if (reset | clear_pipeline) begin
+      execute_valid <= 0;
+      is_wrong_prediction <= 0;
+    end else begin
+      if (!block) begin
+        execute_valid <= decode_valid;
+        is_wrong_prediction <= execute_valid && npc_sel_buffer != 0;
+
+        opcode <= alu_opcode;
+        operand1 <= alu_operand1_sel[0] ? exu_r_wdata : alu_operand1_sel[1] ? forward_data : src1;
+        operand2 <= alu_operand2_sel[0] ? imm : alu_operand2_sel[1] ? csr_src_tmp : src2_tmp;
+
+        snpc <= idu_pc + 4;
+        dnpc <= idu_pc + imm;
+
+        exu_pc <= idu_pc;
+        exu_npc <= exu_npc_buffer;
+
+        wsrc <= src2_tmp;
+
+        npc_sel_buffer <= npc_sel;
+
+        csr_src_buffer <= csr_src_tmp;
+
+        exu_r_wdata_sel_buffer <= exu_r_wdata_sel;
+
+        csr_wdata <= alu_result;
+      end
+    end
+  end
 
 endmodule
