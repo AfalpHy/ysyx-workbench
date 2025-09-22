5539c51fe00e00a3caf418fd6b831d9b5c0d327b
tracer-ysyx <tracer@ysyx.org>
2025-09-22 17:11:08 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:11:08 up 23 min,  1 user,  load average: 0.14, 0.18, 0.11

--- npc/csrc/pmem.cpp
+++ npc/csrc/pmem.cpp
@@ -84,6 +84,8 @@ extern "C" void psram_read(paddr_t addr, int *data) {
     fprintf(log_fp, "read addr:\t" FMT_PADDR "\tdata:" FMT_WORD "\n", addr,
             result);
 #endif
+    printf( "read addr:\t" FMT_PADDR "\tdata:" FMT_WORD "\n", addr,
+            result);
   *data = result;
 }
 
@@ -96,6 +98,10 @@ extern "C" void psram_write(word_t addr, word_t data, int mask) {
             "\n",
             addr, data, mask);
 #endif
+    printf(
+            "write addr:\t" FMT_PADDR "\tdata:" FMT_WORD "\tmask:" FMT_WORD
+            "\n",
+            addr, data, mask);
   uint8_t *psram_addr = (uint8_t *)psram;
   psram_addr += addr;
   word_t origin_data = *(word_t *)psram_addr;
