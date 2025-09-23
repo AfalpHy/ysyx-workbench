3da7fd9caebe8cdb252d70809c330187094c5f80
tracer-ysyx <tracer@ysyx.org>
2025-09-23 14:05:24 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:05:24 up 29 min,  1 user,  load average: 0.15, 0.27, 0.26

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -42,7 +42,7 @@ void fflush_trace() {
 
 void sigint_handler(int sig) {
   // print_debug_info();
-  // print_performance_info();
+  print_performance_info();
 #ifdef TRACE_WAVE
   tfp->close();
 #endif
