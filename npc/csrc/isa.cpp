9ceb5cf8bf06fbf00abcad9ea1eb27fdd487e835
tracer-ysyx <tracer@ysyx.org>
2025-09-24 07:34:53 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  07:34:53 up 56 min,  1 user,  load average: 0.47, 0.37, 0.30

--- npc/csrc/isa.cpp
+++ npc/csrc/isa.cpp
@@ -3,16 +3,11 @@
 #include <string.h>
 
 word_t *regs = nullptr;
-paddr_t *pc = nullptr;
 
 const char *regs_name[] = {"$0", "ra", "sp",  "gp",  "tp", "t0", "t1", "t2",
                            "s0", "s1", "a0",  "a1",  "a2", "a3", "a4", "a5",
                            "a6", "a7", "s2",  "s3",  "s4", "s5", "s6", "s7",
                            "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
-
-extern "C" void set_pc(const svOpenArrayHandle r) {
-  pc = (paddr_t *)(((VerilatedDpiOpenVar *)r)->datap());
-}
 
 extern "C" void set_regs_ptr(const svOpenArrayHandle r) {
   regs = (word_t *)(((VerilatedDpiOpenVar *)r)->datap());
