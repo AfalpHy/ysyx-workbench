ebae87ff7ebe71544aa8e9df5871c0587fd44954
tracer-ysyx <tracer@ysyx.org>
2025-09-24 07:10:53 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  07:10:53 up 32 min,  1 user,  load average: 0.68, 0.91, 0.50

--- npc/csrc/exec.cpp
+++ npc/csrc/exec.cpp
@@ -203,7 +203,7 @@ static int check_regs() {
   word_t ref_reg[REGS_NUM];
   paddr_t ref_pc;
   ref_difftest_regcpy((void *)ref_reg, &ref_pc, DIFFTEST_TO_DUT);
-  if (current_npc != ref_pc) {
+  if (!halt && current_npc != ref_pc) {
     std::cerr << std::hex << " ref pc:" << ref_pc << " npc:" << current_npc
               << std::endl;
     return -1;
@@ -274,9 +274,6 @@ void cpu_exec(uint32_t num) {
               total_insts_num); // make trace more clear
 #endif
 
-    if (halt) {
-      return;
-    }
     if (diff_test_on) {
       if (skip_ref_inst) {
         ref_difftest_regcpy(regs, &current_npc, DIFFTEST_TO_REF);
@@ -289,7 +286,9 @@ void cpu_exec(uint32_t num) {
         }
       }
     }
-    if (check_wp()) {
+    if (halt) {
+      return;
+    } else if (check_wp()) {
       return;
     } else if (check_breakpoint(current_pc)) {
       return;
