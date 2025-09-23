ac68b8c13296ead5b1004fdf021e4d58dd7e3525
tracer-ysyx <tracer@ysyx.org>
2025-09-23 16:10:39 +0800

    > sim RTL ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:10:39 up  2:34,  1 user,  load average: 0.51, 1.38, 1.18

--- npc/vsrc/Arbiter.v
+++ npc/vsrc/Arbiter.v
@@ -96,6 +96,7 @@ module ysyx_25010008_Arbiter (
   assign io_master_wstrb = wstrb_1;
   assign io_master_wvalid = state == CHOSE_LSU ? wvalid_1 : 0;
   assign io_master_bready = state == CHOSE_LSU ? bready_1 : 0;
+  assign io_master_awburst = 2'b01;
   assign io_master_wlast = 1;
 
   assign arready_0 = state == CHOSE_IFU ? io_master_arready : 0;
