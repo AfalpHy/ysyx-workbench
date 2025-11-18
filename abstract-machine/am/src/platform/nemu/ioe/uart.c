d8db250ac84631bf093f7f994ba7f1f97bf2bd0f
tracer-ysyx <tracer@ysyx.org>
2025-11-18 16:39:43 +0800

    > compile NEMU ysyx_25010008 吴杭宇 Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:39:43 up 26 min,  1 user,  load average: 0.17, 0.16, 0.11

--- abstract-machine/am/src/platform/nemu/ioe/uart.c
+++ abstract-machine/am/src/platform/nemu/ioe/uart.c
@@ -1,4 +1,7 @@
 #include <am.h>
 #include <klib.h>
 
-void __am_uart_rx(AM_UART_RX_T *uart) { uart->data = 0xff; }
+void __am_uart_rx(AM_UART_RX_T *uart) {
+  // this function is not important on nemu, always return -1
+  uart->data = 0xff;
+}
